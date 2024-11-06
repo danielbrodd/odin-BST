fe84c45f98d42e36989a0cba5013af6c5dc7e5c0
danielbrodd <daniel.brodd@gmail.com>
2024-11-06 11:28:01 +0100

    add removeItem and helper _getSuccessor
    removeItem recursively traverses the binary search tree and removes one node, and makes sure it remains a BST
    _getSuccessor helper function to travers the binary search tree and find the next in order node (smallest value greater than current)

---
+++ main.js
@@ -0,0 +1,32 @@
+import Tree from "./tree.js";
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+
+let array = [10,20,30,40,50,80,70,60]
+
+let tree = new Tree(array)
+
+
+tree.insert(90)
+
+
+
+prettyPrint(tree.root)
+
+tree.deleteItem(50)
+
+prettyPrint(tree.root)
+
+let res = tree.find(80)
+console.log(`res: ${res}`)
--- tree.js
+++ tree.js
@@ -3,7 +3,7 @@ import Node from "./node.js";
 class Tree {
     constructor(array){
         const sortedArray = [... new Set(array)].sort((a,b) => a - b);
-        this.node = this.buildTree(sortedArray);
+        this.root = this.buildTree(sortedArray);
     }
 
     buildTree(array) {
@@ -21,15 +21,58 @@ class Tree {
 
         return node
     }
-    insert(value, node = this.root) {
-        if (node === null) node = new Node(value);
+    insert(data, node = this.root) {
+        if (node === null) return new Node(data);
 
-        if (node.data === value) throw new Error("No duplicates allowed");
+        if (node.data === data) throw new Error("No duplicates allowed");
 
-        if (value < node.data) {
-            node.left = this.insert(value, node.left)
-        } else if (value > node.data) {
-            node.right = this.insert(value, node.right)
+        if (data < node.data) {
+            node.left = this.insert(data, node.left)
+        } else if (data > node.data) {
+            node.right = this.insert(data, node.right)
         }
+        return node
+    }
+    deleteItem(data, node = this.root) {
+        if (node === null) return node;
+
+        if (data < node.data) {
+            node.left = this.deleteItem(data, node.left);
+        } else if (data > node.data) {
+            node.right = this.deleteItem(data, node.right);
+        } else {
+            if (node.left === null) return node.right;
+
+            if (node.right === null) return node.left;
+
+            let successor = this._getSuccessor(node);
+
+            node.data = successor.data;
+            node.right = this.deleteItem(successor.data, node.right);
+        }
+        return node
+    }
+    find(data, node = this.root) {
+        if (node === null) return node;
+
+        if (data < node.data) {
+            this.find(data, node.left);
+        } else if (data > node.data){
+            this.find(data, node.right)
+        } else {
+            console.log(node)
+            return node
+        }
+        return
+    }
+    _getSuccessor(curr) {
+        curr = curr.right;
+
+        while (curr !== null && curr.left !== null) {
+            curr = curr.left;
+        }
+        return curr;
     }
 }
+
+export default Tree;
